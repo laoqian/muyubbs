c2d3795062b41a1b7f56bea43412a571b3418367
858385135@qq.com <858385135@qq.com>
2015-12-28 19:11:44 +0800

    修改一点点

--- Controller/IndexController.class.php
+++ Controller/IndexController.class.php
@@ -366,7 +366,7 @@ class IndexController extends Controller {
     }
 
     if($step<=1 || $step>5 || !$step){
-      $this->display("template/register-step-5");
+      $this->display("template/register-step-2");
     }else if($step == 2){
       $this->display("template/register-step-2");
     }else if($step == 3){
