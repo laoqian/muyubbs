edd6cb267bea08856f68e96c5efbfb02865a8916
858385135@qq.com <858385135@qq.com>
2015-12-28 14:14:17 +0800

    验证添加登录时候，以及将QQ设置为必须添加的

--- Controller/IndexController.class.php
+++ Controller/IndexController.class.php
@@ -340,7 +340,7 @@ class IndexController extends Controller {
     }
 
     if($step<=1 || $step>5 || !$step){
-      $this->display("template/register-step-4");
+      $this->display("template/register-step-1");
     }else if($step == 2){
       $this->display("template/register-step-2");
     }else if($step == 3){
